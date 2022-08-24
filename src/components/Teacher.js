e7e3ce0e1c8b2c41bedcb167559e327e23f6ccc1
Pedro <tampocosequeponerenesteespacio@gmail.com>
2022-08-24 08:24:07 -0400

    fixed hardcoded period state

--- src/components/Teacher.js
+++ src/components/Teacher.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { v4 as uuid } from 'uuid'
+import { whatPeriod } from "../utils/what-period"
 
 
 
 const Teacher = ({teacher, day, date}) => {
 
-    const period = 6
+    const period = whatPeriod(date) //CHECK THIS!!
     const classes = teacher.slice( 12*(day-1) + 1 , 12*(day) + 1)
     
     
